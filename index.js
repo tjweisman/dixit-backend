f4f5dd124daf248003352ac4e39b7b42da574a21
Teddy Weisman <tjweisman@gmail.com>
2020-09-18 14:11:32 -0500

    script to run local server, plus postgresql testing

--- index.js
+++ index.js
@@ -3,6 +3,25 @@ const app = require('express')();
 const http = require('http').createServer(app);
 var io = require('socket.io')(http);
 
+const { Client } = require('pg');
+
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+
+client.connect();
+
+client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
+  if (err) throw err;
+  for (let row of res.rows) {
+    console.log(JSON.stringify(row));
+  }
+  client.end();
+});
+
 
 app.get('/', (req, res) => {
   res.send('');
