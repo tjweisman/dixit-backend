8d730f2f0c294db8ff2dbfcad8a8d725dce22e76
Teddy Weisman <tjweisman@gmail.com>
2020-09-19 02:00:18 -0500

    lots of progress

---
+++ database_structure.sql
@@ -0,0 +1,45 @@
+DROP TABLE IF EXISTS users, games, cards, default_cards;
+
+DROP TYPE IF EXISTS card_state;
+
+CREATE TABLE users(
+	
+	uid SERIAL UNIQUE,
+	name varchar(40) NOT NULL,
+	game varchar(40) NOT NULL,
+	score INTEGER DEFAULT 0,
+	turn_order SERIAL,
+	PRIMARY KEY(uid),
+
+	CONSTRAINT user_room UNIQUE(name, game)
+);
+
+CREATE TABLE games(
+	gid SERIAL UNIQUE,
+	name varchar(40) CONSTRAINT name_unique UNIQUE NOT NULL,
+	turn INTEGER,
+	in_progress BOOLEAN DEFAULT FALSE,
+	PRIMARY KEY(gid),
+
+	CONSTRAINT fk_turn FOREIGN KEY(turn) REFERENCES users(uid)
+);
+
+CREATE TABLE default_cards(
+	filename VARCHAR NOT NULL
+);
+
+INSERT INTO default_cards(filename) VALUES ('tmp1.jpg'), ('tmp2.jpg'), ('tmp3.jpg'), ('tmp4.jpg'), ('tmp5.jpg'), ('tmp6.jpg'), ('tmp7.jpg'), ('tmp8.jpg');
+
+CREATE TYPE card_state AS ENUM ('deck', 'hand', 'discard');
+
+CREATE TABLE cards(
+	cid SERIAL UNIQUE,
+	filename VARCHAR,
+	uid INTEGER,
+	gid INTEGER NOT NULL,
+	state card_state NOT NULL DEFAULT 'deck',
+
+	PRIMARY KEY(cid),
+	CONSTRAINT fk_game FOREIGN KEY(gid) REFERENCES games(gid),
+	CONSTRAINT fk_user FOREIGN KEY(uid) REFERENCES users(uid)
+);
--- index.js
+++ index.js
@@ -14,15 +14,6 @@ const client = new Client({
 
 client.connect();
 
-client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
-  if (err) throw err;
-  for (let row of res.rows) {
-    console.log(JSON.stringify(row));
-  }
-  client.end();
-});
-
-
 app.get('/', (req, res) => {
   res.send('');
 });
@@ -31,13 +22,128 @@ http.listen(PORT, () => {
 	console.log("listening...");
 });
 
+async function join_room(socket, username, room, callback) {
+  try {
+    let res = await client.query("INSERT INTO users(name, game) VALUES($1, $2) RETURNING uid;",
+      [username, room]);
+
+    uid = res.rows[0].uid;
+
+    res = await client.query("INSERT INTO games(name, turn) VALUES($1, $2) \
+      ON CONFLICT ON CONSTRAINT name_unique DO NOTHING;",
+      [room, uid]);
+
+    console.log("user " + username + " joined room "+ room);
+  } catch (err) {
+    if(err.code == 23505 && err.constraint == "user_room") {
+      callback({
+        response:"error",
+        error:"user_in_room",
+        username:username,
+        room:room
+      });
+    } else {
+      console.log(err.stack);
+      callback({
+        response:"error",
+        error:"unknown",
+        username:username,
+        room:room
+      });
+    }
+  }
+  callback({
+    response:"success",
+    username:username,
+    room:room
+  });
+  socket.join(room);
+}
+
+function delete_all_data() {
+  client.query("DELETE FROM cards;")
+  .then(res => client.query("DELETE FROM games;"))
+  .then(res => client.query("DELETE FROM users;"));
+}
+
+async function get_room_id(room) {
+  try {
+    const res = await client.query("SELECT gid FROM games WHERE name = $1", [room]);
+    return res.rows[0].gid;
+  } catch(err) {
+    console.log(err.stack);
+  }
+}
+
+async function get_room_name(gid) {
+  try {
+    const res = await client.query("SELECT name FROM games WHERE gid = $1", [gid]);
+    return res.rows[0].name;
+  } catch(err) {
+    console.log(err.stack);
+  }
+}
+
+function setup_cards(gid) {
+  client.query("INSERT INTO cards(filename, gid) (SELECT filename, gid FROM default_cards INNER JOIN games ON $1 = gid);",
+    [gid]);
+}
+
+function begin_game(gid) {
+  client.query("UPDATE games SET in_progress = TRUE WHERE gid = $1;",
+    [gid]);
+  console.log("wow, no errors.");
+}
+
+function deal_cards(gid) {
+  const res = await client.query("SELECT cid, filename FROM cards WHERE gid = $1;", [gid]);
+  name = await get_room_name(gid);
+
+  
+}
+
+async function initialize_room(room) {
+  gid = await get_room_id(room);
+  client.query("DELETE FROM cards WHERE gid = $1", [gid])
+  .then(res => setup_cards(gid))
+  .then(res => begin_game(gid));
+}
+
+async function get_users(room, callback) {
+  try {
+      if(room != "") {
+        var res = await client.query("SELECT * FROM users WHERE room = $1;");
+      } else {
+        var res = await client.query("SELECT * FROM users;");
+      }
+      callback(res.rows);
+  } catch(err) {
+    console.log(err.stack);
+  }
+}
+
 io.set('transports', ['websocket']);
 
 io.on('connection', (socket) => {
 	console.log("New connection.");
-	socket.on("state update", (state) => {
-		console.log("Received status update:");
-		console.log(state);
-		socket.broadcast.emit("state update", state);
+	socket.on("join game", (data, callback) => {
+		
+    console.log("User wants to join room.");
+		console.log(data);
+
+    join_room(socket, data["username"], data["game"], callback);
 	});
+
+  socket.on("delete all", () => {
+    delete_all_data();
+  });
+
+  socket.on("get users", (room, callback) => {
+    get_users(room, callback);
+  });
+
+  socket.on("start game", (room) => {
+    initialize_room(room);
+  });
+
 });
