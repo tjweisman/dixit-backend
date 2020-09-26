4fe753fd1bf5b32092a1ead9d5606cd70dff2f6d
Teddy Weisman <tjweisman@gmail.com>
2020-09-26 02:32:22 -0500

    added more cards

--- database_structure.sql
+++ database_structure.sql
@@ -34,10 +34,47 @@ CREATE TABLE default_cards(
 	filename VARCHAR NOT NULL
 );
 
-INSERT INTO default_cards(filename) VALUES ('card1.png'), ('card2.png'), 
-('card3.png'), ('card4.png'), ('card5.png'), ('card6.png'), ('card7.png'), ('card8.png'),
-('card9.png'), ('card10.png'), ('card11.png'), ('card12.png'), ('card13.png'), ('card14.png'),
-('card15.png');
+INSERT INTO default_cards(filename) VALUES 
+('teddy_0.png'),
+('teddy_1.png'),
+('teddy_2.png'),
+('teddy_3.png'),
+('teddy_4.png'),
+('teddy_5.png'),
+('teddy_6.png'),
+('teddy_7.png'),
+('teddy_8.png'),
+('teddy_9.png'),
+('teddy_10.png'),
+('teddy_11.png'),
+('teddy_12.png'),
+('teddy_13.png'),
+('teddy_14.png'),
+('neza_0.png'),
+('neza_1.png'),
+('neza_2.png'),
+('neza_3.png'),
+('neza_4.png'),
+('neza_5.png'),
+('cas_0.png'),
+('cas_1.png'),
+('cas_2.png'),
+('cas_3.png'),
+('cas_4.png'),
+('cas_5.png'),
+('cas_6.png'),
+('cas_7.png'),
+('cas_8.png'),
+('cas_9.png'),
+('cas_10.png'),
+('cas_11.png'),
+('cas_12.png'),
+('rok_0.png'),
+('rok_1.png'),
+('rok_2.png'),
+('rok_3.png'),
+('rok_4.png'),
+('rok_5.png');
 
 CREATE TYPE card_state AS ENUM ('deck', 'hand', 'discard');
 
